cb488c8198869a3eedef5ed7e6aa7c02c285a3f1
theforgottengod2012-a11y <theforgottengod2012@gmail.com>
2026-03-12 19:04:04 +0600

    Create api/bot.js for Discord bot entry point

---
+++ api/bot.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+const TOKEN = process.env.DISCORD_TOKEN;
+client.login(TOKEN);
